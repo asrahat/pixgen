7888b0b0cb42738c7761f349a46c458d85aca030
asrahat <asrahat4742@gmail.com>
2026-04-29 09:49:36 +0600

    json readded

--- src/components/TopGenaration.jsx
+++ src/components/TopGenaration.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const TopGenaration = async() => {
-    const res = await fetch('https://pixgen-sable.vercel.app/data.json');
-    const photos = await res.json();
-    console.log(photos,'photos');
+    
     return (
         <div>
             
